892bd3a9f031c12d395c6a30a32f3db4359e23ea
luis <luismatos138@gmail.com>
2023-11-13 23:39:31 +0000

    more styling, trying to make the bars appear

--- nicecatbadcat/src/components/CatsCard.jsx
+++ nicecatbadcat/src/components/CatsCard.jsx
@@ -6,6 +6,7 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Box, Button, CardActionArea, CardActions } from '@mui/material';
+import { Progress } from 'flowbite-react';
 
 
 
@@ -13,7 +14,7 @@ import { Box, Button, CardActionArea, CardActions } from '@mui/material';
 function CatsCard(props) {
 
   const avarageWeight = ((props.data.max_weight + props.data.min_weight) / 2)
-  const avarageLifeExpectency = ((props.data.max_life_expectancy + props.data.min_life_expectancy ) /2)
+  const avarageLifeExpectency = ((props.data.max_life_expectancy + props.data.min_life_expectancy) / 2)
   // const catsProperties = {
 
 
@@ -37,8 +38,8 @@ function CatsCard(props) {
   console.log(props.data)
 
   return (
-
-    <Card sx={{ width: 450, border: 2, margin: 2, }}>
+<>
+    <Card sx={{ width: 450, border: 2, margin: 2 }}>
       <CardActionArea>
         <CardActions>
           <Button size="small" color="primary">
@@ -55,31 +56,38 @@ function CatsCard(props) {
         />
         <CardContent>
 
-          <Box sx={{display:"flex", justifyContent:"space-evenly", flexDirection:"column"}}>
+          <Box sx={{ display: "flex", justifyContent: "space-evenly", flexDirection: "column" }}>
             <Typography className='title' variant="h4" component="div" align='center' marginBottom={2}>
               {props.data.name}
             </Typography>
             <Typography variant="h5" color="text.secondary" marginBottom={2} align='center'>
-              {props.data.origin}
+              From: {props.data.origin}
             </Typography>
-            <Box sx={{display:"flex", justifyContent:"space-between"}}>
-            <Typography >
-              Avarage Weight: {avarageWeight}
-            </Typography>
-            <Typography>
-              Length: {props.data.length}
-            </Typography>
+            <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+              <Typography >
+                Avarage Weight: {avarageWeight}
+              </Typography>
+              <Typography>
+                Length: {props.data.length}
+              </Typography>
             </Box>
             <Typography align='center' margin={2}>
               Average Life Expectancy: {avarageLifeExpectency}
             </Typography>
           </Box>
+        <Box>
+          <div className='progress-bar-parent'>
+            <div className='progress-bar-child'>
+            </div>
+          </div>
+        </Box>
 
         </CardContent>
       </CardActionArea>
 
-    </Card>
 
+    </Card>
+    </>
   )
 }
 
